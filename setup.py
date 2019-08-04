98cace82f5d3501ecb6ff30b12270e18e8bb81f6
Ramtin <ramtin.abadi@gmail.com>
2019-08-04 22:46:38 +0530

    Preparation for Pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: GNU Lesser General Public License v3.0 or later (LGPL-3.0-or-later)",
         "Operating System :: OS Independent",
     ],
 )
